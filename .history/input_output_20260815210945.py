f1678d5ca54378f504af12d2f3d8e1098b2ce370
sanv108 <vsandeepdas1008@gmail.com>
2026-08-15 21:21:06 +0530

    input & output & formating

---
+++ .history/input_output_20260815210945.py
@@ -0,0 +1,24 @@
+# name = input("Enter your name: ")
+# print("Hello", name)
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print(f"My name is {name} and I am {age} years old.")
+
+x= 23
+g=56
+print("The sum of",x,"and",g,"is",x+g)
+
+
+name = "Sandeep"
+age = 22
+city = "Gorakhpur"
+
+print(name, age, city, sep=", ")
+
+print("Python", end=" | ")
+print("JavaScript", end=" & ")
+print("C++")
+
+
---
+++ .history/input_output_20260815211547.py
@@ -0,0 +1,48 @@
+# name = input("Enter your name: ")
+# print("Hello", name)
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print(f"My name is {name} and I am {age} years old.")
+
+x= 23
+g=56
+print("The sum of",x,"and",g,"is",x+g)
+
+
+name = "Sandeep"
+age = 22
+city = "Gorakhpur"
+
+print(name, age, city, sep=", ")
+
+print("Python", end=" | ")
+print("JavaScript", end=" & ")
+print("C++")
+
+
+#using f-string
+name = input("Enter your name: ")
+
+print(f"Hello, {name}!")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"My name is {name} and I am {age} years old.")
+
+
+
+#.formate string
+name = "Sandeep"
+age = 22
+
+print("My name is {} and I am {} years old.".format(name, age))
+
+
+#%s and %d
+name = "Sandeep"
+age = 22
+
+print("My name is %s and I am %d years old." % (name, age))
