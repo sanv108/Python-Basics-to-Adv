2223a5c500e1d4149b5c330ea49277bf84c73052
sanv108 <vsandeepdas1008@gmail.com>
2026-08-15 20:08:55 +0530

    Data  types in python

---
+++ Data_type.py
@@ -0,0 +1,20 @@
+x = "Hello World" #str 
+x = 200 #int 
+x = 20.59 #float 
+x = 1j #complex 
+x = ["apple", "banana", "cherry"] #list 
+x = ("apple", "banana", "cherry") #tuple 
+x = range(6) #range 
+x = {"name" : "John", "age" : 36} #dict 
+x = {"apple", "banana", "cherry"} #set 
+x = frozenset({"apple", "banana", "cherry"}) #frozenset 
+x = {"apple", "banana", "cherry"} #tuple
+
+print(x)
+print(type(x))
+
+# x = True #bool 
+# x = b"Hello" #bytes 
+# x = bytearray(5) #bytearray 
+# x = memoryview(bytes(5)) #memoryview 
+# x = None
